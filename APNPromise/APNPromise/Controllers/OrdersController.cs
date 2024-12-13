e35a7fe0fd1672a8652383d56e8333f9f285dae4
marekmical <56329747+marekmical@users.noreply.github.com>
2024-12-13 14:07:45 +0100

    Add controllers for Books and Orders (#3)
    Added Get and Post endpoints for api/Books
    Added Get endpoint for api/Orders

---
+++ APNPromise/APNPromise/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APNPromise.Models;
+
+namespace APNPromise.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly OrdersContext _context;
+
+        public OrdersController(OrdersContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrdersList()
+        {
+            return await _context.OrdersList.Include(x => x.OrderLines).AsNoTracking().ToListAsync();
+        }
+    }
+}
