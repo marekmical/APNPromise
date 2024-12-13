8c2ac83a2eda9821da78fc7317f134d8e992326f
marekmical <marek.mical@gmail.com>
2024-12-13 14:07:04 +0100

    Add controllers for Books and Orders
    Added Get and Post endpoints for api/Books
    Added Get endpoint for api/Orders

---
+++ APNPromise/APNPromise/Controllers/BooksController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APNPromise.Models;
+
+namespace APNPromise.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksContext _context;
+
+        public BooksController(BooksContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Books
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooksList()
+        {
+            return await _context.BooksList.Include(x => x.Authors).ToListAsync();
+        }
+
+
+        // POST: api/Books
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBook(Book book)
+        {
+            try
+            {
+                _context.BooksList.Add(book);
+                await _context.SaveChangesAsync();
+            }
+            catch (ArgumentException argEx)
+            {
+                return new CreatedResult("Book with given ID already exists", 400);
+            }
+
+            return Created();
+        }
+    }
+}
